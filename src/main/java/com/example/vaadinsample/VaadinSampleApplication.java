f444f87a61114833e0f32b58a42b868419a95caa
youngyin <57608725+youngyin@users.noreply.github.com>
2025-01-05 14:15:55 +0900

    Initial commit

---
+++ src/main/java/com/example/vaadinsample/VaadinSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.vaadinsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaadinSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaadinSampleApplication.class, args);
+	}
+
+}
